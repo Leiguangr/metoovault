15fe5590ab9c096d5a9da0afba6d1b74e384b747
Tony Ren <tren@guardiananalytics.com>
2018-01-14 00:18:59 -0800

    clientside interaction with contract

--- src/js/app.js
+++ src/js/app.js
@@ -31,23 +31,23 @@ App = {
     },
 
     initContract: function() {
-        $.getJSON('Adoption.json', function(data) {
+        $.getJSON('TweetAccount.json', function(data) {
             // Get the necessary contract artifact file and instantiate it with truffle-contract
-            var AdoptionArtifact = data;
-            App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+            var VaultArtifact = data;
+            App.contracts.Vault = TruffleContract(VaultArtifact);
 
             // Set the provider for our contract
-            App.contracts.Adoption.setProvider(App.web3Provider);
+            App.contracts.Vault.setProvider(App.web3Provider);
 
             // Use our contract to retrieve and mark the adopted pets
-            return App.markAdopted();
+            //return App.EncryptAndPersist();
         });
 
         return App.bindEvents();
     },
 
     bindEvents: function() {
-        $(document).on('click', '.btn-adopt', App.handleAdopt);
+        $(document).on('click', '.btn-adopt', App.encryptAndPersist);
     },
 
     markAdopted: function(adopters, account) {
@@ -68,68 +68,12 @@ App = {
         });
     },
 
-    handleAdopt1: function(event) {
+    encryptAndPersist: function(event) {
         event.preventDefault();
 
         var petId = parseInt($(event.target).data('id'));
-        var adoptionInstance;
 
-        web3.eth.getAccounts(function(error, accounts) {
-            if (error) {
-                console.log(error);
-            }
-
-            var account = accounts[0];
-
-            App.contracts.Adoption.deployed().then(function(instance) {
-                adoptionInstance = instance;
-
-                // Execute adopt as a transaction by sending account
-                return adoptionInstance.adopt(petId, {from: account});
-            }).then(function(result) {
-                return App.markAdopted();
-            }).catch(function(err) {
-                console.log(err.message);
-            });
-        });
-    },
-
-
-    handleAdopt: function(event) {
-        event.preventDefault();
-
-        var petId = parseInt($(event.target).data('id'));
-        var adoptionInstance;
         var message = document.getElementById("tellyourstory").value;
-
-        var params = { keyBytes: 32, ivBytes: 16 };
-        var dk = window.keythereum.create(params);
-
-
-        const { randomBytes } = require('crypto');
-        const secp256k1 = require('secp256k1')
-// or require('secp256k1/elliptic')
-//   if you want to use pure js implementation in node
-
-// generate message to sign
-        const msg = randomBytes(32)
-
-// generate privKey
-        let privKey
-        do {
-            privKey = randomBytes(32)
-        } while (!secp256k1.privateKeyVerify(privKey))
-
-// get the public key in a compressed format
-        const pubKey = secp256k1.publicKeyCreate(privKey)
-
-// sign the message
-        const sigObj = secp256k1.sign(msg, privKey)
-
-// verify the signature
-        console.log(secp256k1.verify(msg, sigObj.signature, pubKey))
-
-
 
         var encrypt = new JSEncrypt();
         encrypt.setPublicKey($('#pubkey').val());
@@ -140,7 +84,38 @@ App = {
         decrypt.setPrivateKey($('#privkey').val());
         var uncrypted = decrypt.decrypt(encrypted);
 
+        console.log(message);
 
+        var vaultInstance;
+
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+            var account = accounts[0];
+
+            console.log(account);
+            var abiDefinition = JSON.parse('[{"constant":true,"inputs":[],"name":"getLatestTweet","outputs":[{"name":"tweetString","type":"string"},{"name":"timestamp","type":"uint256"},{"name":"numberOfTweets","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tweetId","type":"uint256"}],"name":"getTweet","outputs":[{"name":"tweetString","type":"string"},{"name":"timestamp","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getNumberOfTweets","outputs":[{"name":"numberOfTweets","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tweetString","type":"string"}],"name":"tweet","outputs":[{"name":"result","type":"int256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]');
+            var VaultClass = web3.eth.contract(abiDefinition);
+            var VaultInstance = VaultClass.at('0x901d35ab95269e0f777a6b2e284ff9e4f4ce367b');
+            VaultInstance.tweet(message, (error, result) => (console.log(result)));
+            
+           // myContract.(message).call({from: account}).then(console.log);
+
+            /*
+            App.contracts.Vault.deployed().then(function(instance) {
+                vaultInstance = instance;
+                // Execute adopt as a transaction by sending account
+                return vaultInstance.tweet(message, {from: account});
+            }).then(function(result) {
+                console.log(vaultInstance.getTweet());
+            }).catch(function(err) {
+                console.log(err.message);
+            }); */
+
+
+
+        });
 
     }
 
