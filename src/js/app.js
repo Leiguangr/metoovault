1268aeeefb1acf329d3934a01b90cefbadffef2f
Tony Ren <tren@guardiananalytics.com>
2018-01-13 22:11:05 -0800

    update

--- src/js/app.js
+++ src/js/app.js
@@ -105,8 +105,31 @@ App = {
         var params = { keyBytes: 32, ivBytes: 16 };
         var dk = window.keythereum.create(params);
 
-        var private_key = dk.privateKey;
-        var public_key = dk.
+
+        const { randomBytes } = require('crypto');
+        const secp256k1 = require('secp256k1')
+// or require('secp256k1/elliptic')
+//   if you want to use pure js implementation in node
+
+// generate message to sign
+        const msg = randomBytes(32)
+
+// generate privKey
+        let privKey
+        do {
+            privKey = randomBytes(32)
+        } while (!secp256k1.privateKeyVerify(privKey))
+
+// get the public key in a compressed format
+        const pubKey = secp256k1.publicKeyCreate(privKey)
+
+// sign the message
+        const sigObj = secp256k1.sign(msg, privKey)
+
+// verify the signature
+        console.log(secp256k1.verify(msg, sigObj.signature, pubKey))
+
+
 
         var encrypt = new JSEncrypt();
         encrypt.setPublicKey($('#pubkey').val());
